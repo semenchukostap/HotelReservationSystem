83c08bac748d6fa59103844a48810ef6bc6bfb98
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-09 04:58:18 -0700

    Refactor HomeController.cs for migration to .NET 8

--- HotelReservationSystem/Controllers/HomeController.cs
+++ HotelReservationSystem/Controllers/HomeController.cs
@@ -3,21 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HotelReservationSystem.Services;
 
 namespace HotelReservationSystem.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        //[OutputCache(Duration = 50, Location = System.Web.UI.OutputCacheLocation.Server, VaryByParam = "*")]
+        private readonly IHomeService _homeService;
+
+        public HomeController(IHomeService homeService)
+        {
+            _homeService = homeService;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var viewModel = _homeService.GetIndexViewModel();
+            return View(viewModel);
         }
 
         public ActionResult About()
         {
-            return View();
+            var viewModel = _homeService.GetAboutViewModel();
+            return View(viewModel);
         }
+    }
+}
+
+namespace HotelReservationSystem.Services
+{
+    public interface IHomeService
+    {
+        IndexViewModel GetIndexViewModel();
+        AboutViewModel GetAboutViewModel();
+    }
+
+    public class HomeService : IHomeService
+    {
+        public IndexViewModel GetIndexViewModel()
+        {
+            // Prepare the data for the Index view
+            return new IndexViewModel();
+        }
+
+        public AboutViewModel GetAboutViewModel()
+        {
+            // Prepare the data for the About view
+            return new AboutViewModel();
+        }
+    }
+
+    public class IndexViewModel
+    {
+        // Properties and methods for the Index view model
+    }
+
+    public class AboutViewModel
+    {
+        // Properties and methods for the About view model
     }
 }
