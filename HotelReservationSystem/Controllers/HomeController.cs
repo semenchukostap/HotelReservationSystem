0514416a5a72fa00188d3ba18ec0e3cd59cb0578
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-12 01:51:40 -0700

    Updated HomeController.cs for ASP.NET Core compatibility.

--- HotelReservationSystem/Controllers/HomeController.cs
+++ HotelReservationSystem/Controllers/HomeController.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelReservationSystem.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        //[OutputCache(Duration = 50, Location = System.Web.UI.OutputCacheLocation.Server, VaryByParam = "*")]
-        public ActionResult Index()
+        //[ResponseCache(Duration = 50, Location = ResponseCacheLocation.Any, VaryByParam = "*")]
+        public IActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public IActionResult About()
         {
             return View();
         }
