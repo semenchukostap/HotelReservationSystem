6e25120ac89701f7ef4d481854b4509e0e5372f6
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-12 01:53:45 -0700

    Updated ApplicationDbContext to use Entity Framework Core

--- HotelReservationSystem/Models/ApplicationDbContext.cs
+++ HotelReservationSystem/Models/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,20 @@ namespace HotelReservationSystem.Models
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            // Remove pluralizing table name convention
+            builder.RemovePluralizingTableNameConvention();
+        }
+    }
+
+    public static class ModelBuilderExtensions
+    {
+        public static void RemovePluralizingTableNameConvention(this ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(entity.DisplayName());
+            }
         }
     }
 }
