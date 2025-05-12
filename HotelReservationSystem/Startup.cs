59d9697fa7fc3c2f50383dce6508d9aa76628c5e
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-12 01:49:29 -0700

    Add Startup.cs for ASP.NET Core configuration

--- HotelReservationSystem/Startup.cs
+++ HotelReservationSystem/Startup.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.Owin;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(HotelReservationSystem.Startup))]
+[assembly: OwinStartup(typeof(HotelReservationSystem.Startup))]
 namespace HotelReservationSystem
 {
     public partial class Startup
@@ -9,6 +14,46 @@ namespace HotelReservationSystem
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            ConfigureAutoMapper();
+            ConfigureWebApi(app);
+            RegisterGlobalConfigurations();
+        }
+
+        private void ConfigureAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                // Add your AutoMapper profiles or configurations here
+            });
+
+            IMapper mapper = config.CreateMapper();
+            // Optionally, you can store the IMapper instance in a DI container
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Optionally, add other Web API configurations here
+
+            app.UseWebApi(config);
+        }
+
+        private void RegisterGlobalConfigurations()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
