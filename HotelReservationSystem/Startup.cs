4e4a4c4d8a5bf1d451fcbc35215c8cb9fc1a7daf
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-12 01:53:36 -0700

    Created Startup.cs to configure services and middleware

--- HotelReservationSystem/Startup.cs
+++ HotelReservationSystem/Startup.cs
@@ -21,7 +21,6 @@ namespace HotelReservationSystem
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -50,7 +49,6 @@ namespace HotelReservationSystem
                 });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
