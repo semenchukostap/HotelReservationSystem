bc1b5f7c7b5d3a0c229dbbf92222f7c8338746cf
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 07:42:54 -0700

    create_rolename_constants_for_net8_migration

---
+++ Models/RoleName.cs
@@ -0,0 +1,8 @@
+namespace HotelReservationSystem.Models
+{
+    public static class RoleName
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
