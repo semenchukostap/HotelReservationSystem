a46645d60edad3839c6f1cefec09347605820e6f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 07:41:56 -0700

    Create HomeController.cs with ASP.NET Core patterns

---
+++ HotelReservationSystem.Core/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using HotelReservationSystem.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HotelReservationSystem.Core.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
