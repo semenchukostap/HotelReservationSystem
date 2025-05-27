6ea8d03bf58ea21ebc46ab80623aac1d1ed89139
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 07:42:48 -0700

    create_applicationuser_model_for_net8_migration

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Phone { get; set; }
+    }
+}
