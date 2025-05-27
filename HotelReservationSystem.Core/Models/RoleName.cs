ea29c2bc97f07d779499908d1ecc5e5004841588
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 07:48:56 -0700

    Migrate RoleName class to .NET 8 and add additional role constants

---
+++ HotelReservationSystem.Core/Models/RoleName.cs
@@ -0,0 +1,9 @@
+namespace HotelReservationSystem.Core.Models
+{
+    public static class RoleName
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string CanManageHotels = "CanManageHotels";
+    }
+}
