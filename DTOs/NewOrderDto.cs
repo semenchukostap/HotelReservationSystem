581171ba72d5d7ce7dc6d526748edd4dc7d57430
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-27 07:46:39 -0700

    create_neworderdto_for_net8_migration

---
+++ DTOs/NewOrderDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.DTOs
+{
+    public class NewOrderDto
+    {
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int HotelId { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public int NumberOfDays { get; set; }
+    }
+}
