93669eb8f445bff157a30d3bdad8bb629e48bd94
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 08:45:00 -0700

    Add RoleNames constants class for authentication roles

---
+++ new_app/HotelReservationSystem.Core/Constants/RoleNames.cs
@@ -0,0 +1,8 @@
+namespace HotelReservationSystem.Core.Constants
+{
+    public static class RoleNames
+    {
+        public const string Administrator = "Administrator";
+        public const string CanManageHotels = "CanManageHotels";
+    }
+}
