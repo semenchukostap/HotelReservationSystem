9485df37dbe99ecae2d4ed33adc5d79e8e40199e
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 12:42:52 -0700

    QA: Updated HotelDto with nullability annotations and modern C# syntax

--- new_app/DTOs/HotelDto.cs
+++ new_app/DTOs/HotelDto.cs
@@ -2,31 +2,58 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HotelReservationSystem.DTOs
 {
+    /// <summary>
+    /// Data transfer object for hotel information
+    /// </summary>
     public class HotelDto
     {
+        /// <summary>
+        /// The unique identifier of the hotel
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The name of the hotel
+        /// </summary>
         [Required]
         [MaxLength(255)]
         public string Name { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The country identifier where the hotel is located
+        /// </summary>
         [Required]
         public int CountryId { get; set; }
         
+        /// <summary>
+        /// The name of the country where the hotel is located
+        /// </summary>
         public string? CountryName { get; set; }
 
+        /// <summary>
+        /// The city where the hotel is located
+        /// </summary>
         [Required]
         [MaxLength(50)]
         public string City { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The star rating of the hotel (1-5)
+        /// </summary>
         [Required]
         [Range(1, 5)]
         public int Stars { get; set; }
 
+        /// <summary>
+        /// The price per night for a standard room
+        /// </summary>
         [Required]
         [Range(1, 1000)]
         public double PricePerNight { get; set; }
 
+        /// <summary>
+        /// Indicates whether the hotel offers all-inclusive packages
+        /// </summary>
         [Required]
         public bool IsAllInclusive { get; set; }
     }
