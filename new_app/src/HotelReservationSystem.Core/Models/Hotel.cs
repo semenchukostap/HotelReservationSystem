3faab02588db20b241f59c05851ed996c78d8450
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 08:43:14 -0700

    Create Hotel domain model

---
+++ new_app/src/HotelReservationSystem.Core/Models/Hotel.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Core.Models;
+
+public class Hotel
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [Display(Name = "Country")]
+    public int CountryId { get; set; }
+    public Country? Country { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string City { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, 5)]
+    public int Stars { get; set; }
+
+    [Required]
+    [Range(1, 1000)]
+    public double PricePerNight { get; set; }
+
+    [Required]
+    public bool IsAllInclusive { get; set; }
+}
