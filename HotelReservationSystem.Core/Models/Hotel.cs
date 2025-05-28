c9918f126c3b8f4ccdae18d780979826324790f8
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 09:09:52 -0700

    QA: Create Hotel model with proper data types and nullable annotations

---
+++ HotelReservationSystem.Core/Models/Hotel.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace HotelReservationSystem.Core.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(255)]
+        public string City { get; set; } = string.Empty;
+        
+        [Required]
+        public int CountryId { get; set; }
+        public Country? Country { get; set; }
+        
+        [Range(1, 5)]
+        public byte Stars { get; set; }
+        
+        public bool IsAllInclusive { get; set; }
+        
+        [Required]
+        [Range(0, 10000)]
+        public decimal PricePerNight { get; set; }
+    }
+}
