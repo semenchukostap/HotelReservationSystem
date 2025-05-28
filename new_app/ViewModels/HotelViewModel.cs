ffe1ec5ea1eae984fa630f5e6c6e272066e2a92a
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:44:11 -0700

    Migrate HotelViewModel to .NET 8 project

---
+++ new_app/ViewModels/HotelViewModel.cs
@@ -0,0 +1,10 @@
+using new_app.Models;
+
+namespace new_app.ViewModels
+{
+    public class HotelViewModel
+    {
+        public Hotel Hotel { get; set; }
+        public IEnumerable<Country> Countries { get; set; }
+    }
+}
