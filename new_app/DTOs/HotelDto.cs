222df05c8314b293220ce6c6a4a6e285e34d8b5e
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:44:14 -0700

    Migrate HotelDto class from .NET 4.5.2 to .NET 8
    - Updated namespace to new_app.DTOs
    - Removed System.Web references
    - Removed Country navigation property to prevent circular references
    - Added proper validation attributes following .NET 8 standards

---
+++ new_app/DTOs/HotelDto.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace new_app.DTOs
+{
+    public class HotelDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Stars { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public double PricePerNight { get; set; }
+
+        [Required]
+        public bool IsAllInclusive { get; set; }
+    }
+}
