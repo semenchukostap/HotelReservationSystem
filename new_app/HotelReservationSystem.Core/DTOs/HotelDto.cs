08bdcdee7fc4645b902680b6d3c91deda82341a4
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 04:28:07 -0700

    Created HotelDto class in Core project

---
+++ new_app/HotelReservationSystem.Core/DTOs/HotelDto.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Core.DTOs
+{
+    public class HotelDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int CountryId { get; set; }
+
+        public CountryDto? Country { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 5)]
+        public int Stars { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public double PricePerNight { get; set; }
+
+        [Required]
+        public bool IsAllInclusive { get; set; }
+    }
+}
