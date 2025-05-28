8fc7f19aece1da7ba2a35aaa5b4614cbc619d8e3
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 12:46:17 -0700

    QA: Update NewOrderDto with proper nullability annotations and modern C# syntax

--- new_app/DTOs/NewOrderDto.cs
+++ new_app/DTOs/NewOrderDto.cs
@@ -12,31 +12,35 @@ namespace HotelReservationSystem.DTOs
         /// The ID of the customer making the reservation
         /// </summary>
         [Required(ErrorMessage = "Customer ID is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Customer ID must be greater than 0")]
         public int CustomerId { get; set; }
 
         /// <summary>
         /// The ID of the hotel being reserved
         /// </summary>
         [Required(ErrorMessage = "Hotel ID is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Hotel ID must be greater than 0")]
         public int HotelId { get; set; }
 
         /// <summary>
         /// The start date of the reservation
         /// </summary>
         [Required(ErrorMessage = "Start date is required")]
+        [DataType(DataType.Date, ErrorMessage = "Start date must be a valid date")]
         public DateTime StartDate { get; set; }
 
         /// <summary>
         /// The end date of the reservation
         /// </summary>
         [Required(ErrorMessage = "End date is required")]
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date, ErrorMessage = "End date must be a valid date")]
         public DateTime EndDate { get; set; }
 
         /// <summary>
         /// The total number of days for the reservation
         /// This is calculated from StartDate and EndDate
         /// </summary>
+        [Range(1, 365, ErrorMessage = "Reservation duration must be between 1 and 365 days")]
         public int NumberOfDays { get; set; }
     }
 }
