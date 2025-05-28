3684c49f0418dec25649fe12b9aa4bc9a15b48de
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:05:38 -0700

    Migrate ApplicationUser class to ASP.NET Core Identity for .NET 8

--- new_app/Models/ApplicationUser.cs
+++ new_app/Models/ApplicationUser.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace HotelReservationSystem.Models
 {
@@ -8,5 +10,14 @@ namespace HotelReservationSystem.Models
         [Required]
         [MaxLength(20)]
         public required string Phone { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, IdentityConstants.ApplicationScheme);
+            
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 }
