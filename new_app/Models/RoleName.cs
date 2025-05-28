f9d35c34451660bacace48e531ad301832df7494
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:14:51 -0700

    QA: Update RoleName class to use standard ASP.NET Core Identity role names

--- new_app/Models/RoleName.cs
+++ new_app/Models/RoleName.cs
@@ -4,9 +4,4 @@ public static class RoleName
 {
     public const string Admin = "Admin";
     public const string User = "User";
-    public const string Manager = "Manager";
-    public const string Receptionist = "Receptionist";
-    public const string HouseKeeping = "HouseKeeping";
-    public const string Maintenance = "Maintenance";
-    public const string Guest = "Guest";
 }
