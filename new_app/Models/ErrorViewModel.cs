fc4649b75b2d73dc7311d4ed004a0380e84c4f4a
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:19:19 -0700

    Update ErrorViewModel for ASP.NET Core MVC

--- new_app/Models/ErrorViewModel.cs
+++ new_app/Models/ErrorViewModel.cs
@@ -1,35 +1,9 @@
-using System;
-
 namespace new_app.Models
 {
-    /// <summary>
-    /// ViewModel used for displaying error information in ASP.NET Core MVC
-    /// </summary>
     public class ErrorViewModel
     {
-        /// <summary>
-        /// The ID of the request that caused the error
-        /// </summary>
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
-        /// <summary>
-        /// Indicates whether the request ID should be displayed to the user
-        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-        
-        /// <summary>
-        /// The error message to display
-        /// </summary>
-        public string ErrorMessage { get; set; }
-        
-        /// <summary>
-        /// HTTP status code associated with the error
-        /// </summary>
-        public int? StatusCode { get; set; }
-        
-        /// <summary>
-        /// Exception details (only shown in development environment)
-        /// </summary>
-        public Exception Exception { get; set; }
     }
 }
