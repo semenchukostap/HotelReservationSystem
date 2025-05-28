d39cf955751b1ce9638e19a1a4e2489979e5ceb9
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:06:08 -0700

    Refine ApplicationUser for .NET 8 Identity with modern API approach

--- new_app/Models/ApplicationUser.cs
+++ new_app/Models/ApplicationUser.cs
@@ -2,21 +2,38 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
+using System;
 
 namespace HotelReservationSystem.Models
 {
+    /// <summary>
+    /// Represents the application user with additional properties and methods
+    /// </summary>
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// The user's phone number
+        /// </summary>
         [Required]
         [MaxLength(20)]
         public required string Phone { get; set; }
 
+        /// <summary>
+        /// Generates a claims identity for the user with custom claims
+        /// </summary>
+        /// <param name="manager">The user manager responsible for creating the identity</param>
+        /// <returns>A ClaimsIdentity containing user information and claims</returns>
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, IdentityConstants.ApplicationScheme);
+            // Use CreateUserIdentityAsync in .NET 8
+            var userIdentity = new ClaimsIdentity(
+                await manager.GetClaimsAsync(this),
+                IdentityConstants.ApplicationScheme);
             
-            // Add custom user claims here
+            // Add custom user claims
+            userIdentity.AddClaim(new Claim("UserPhone", Phone));
+            userIdentity.AddClaim(new Claim("LastAccessed", DateTime.UtcNow.ToString("o")));
+            
             return userIdentity;
         }
     }
