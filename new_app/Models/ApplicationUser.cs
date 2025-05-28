8d80d14cfb871706d0d96e886bd14f4c409888ee
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:28:23 -0700

    QA: Create ApplicationUser model using ASP.NET Core Identity

--- new_app/Models/ApplicationUser.cs
+++ new_app/Models/ApplicationUser.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
-namespace HotelReservationSystem.Models;
-
-public class ApplicationUser : IdentityUser
+namespace HotelReservationSystem.Models
 {
-    [Required]
-    [MaxLength(20)]
-    public required string Phone { get; set; }
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Phone { get; set; } = string.Empty;
+    }
 }
