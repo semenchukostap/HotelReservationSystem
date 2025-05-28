993ce6b6869239231cba40cbc51a2080a522b7da
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 11:46:11 -0700

    Create Hotel model with modern C# syntax and nullable reference types
    - Updated Hotel.cs with .NET 8 standards
    - Added nullable reference types to Country navigation property
    - Used string.Empty as default for string properties instead of null
    - Maintained all properties from the legacy model
    - Preserved validation attributes

---
+++ new_app/Models/Hotel.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        public Country? Country { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        public int CountryId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 5)]
+        public int Stars { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public double PricePerNight { get; set; }
+
+        [Required]
+        public bool IsAllInclusive { get; set; }
+    }
+}
