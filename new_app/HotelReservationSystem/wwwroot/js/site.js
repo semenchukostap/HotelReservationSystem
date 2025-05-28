3df722af8fbbd65d33913ef8b0f0fdbbdc06fedc
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 09:06:07 -0700

    Create site.js with DataTables initialization and toastr configuration
    - Added client-side JavaScript for initializing DataTables plugin for all table elements
    - Configured toastr notification library with standard settings for user feedback
    - Supporting modern .NET 8 static file organization in wwwroot/js folder

---
+++ new_app/HotelReservationSystem/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    // Initialize any DataTables
+    $('.table').DataTable();
+    
+    // Configure toastr notifications
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+});
