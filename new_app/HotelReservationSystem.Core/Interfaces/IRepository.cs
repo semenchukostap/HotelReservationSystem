5f60e364186457915ae4ccf689cb35562d886a33
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 08:47:24 -0700

    Add generic repository interface

---
+++ new_app/HotelReservationSystem.Core/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem.Core.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
