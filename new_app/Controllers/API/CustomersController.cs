bdf717e2515ff74ab8e2a76c0dca3722d77e7e08
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 13:04:21 -0700

    QA: Add missing System namespace for Exception handling

--- new_app/Controllers/API/CustomersController.cs
+++ new_app/Controllers/API/CustomersController.cs
@@ -3,6 +3,7 @@ using HotelReservationSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
