d0c438bd6b583d570ad9cc79842186ffc9f9833c
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 08:47:39 -0700

    Create CustomerService implementation

---
+++ new_app/src/HotelReservationSystem.Services/Services/CustomerService.cs
@@ -0,0 +1,66 @@
+using AutoMapper;
+using HotelReservationSystem.Core.DTOs;
+using HotelReservationSystem.Core.Models;
+using HotelReservationSystem.Core.Services;
+using HotelReservationSystem.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem.Services.Services;
+
+public class CustomerService : ICustomerService
+{
+    private readonly ApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CustomerService(ApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<CustomerDto>> GetAllCustomersAsync()
+    {
+        var customers = await _context.Customers.ToListAsync();
+        return _mapper.Map<IEnumerable<CustomerDto>>(customers);
+    }
+
+    public async Task<CustomerDto?> GetCustomerByIdAsync(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        return customer != null ? _mapper.Map<CustomerDto>(customer) : null;
+    }
+
+    public async Task<int> CreateCustomerAsync(CustomerDto customerDto)
+    {
+        var customer = _mapper.Map<Customer>(customerDto);
+        
+        _context.Customers.Add(customer);
+        await _context.SaveChangesAsync();
+        
+        return customer.Id;
+    }
+
+    public async Task UpdateCustomerAsync(int id, CustomerDto customerDto)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        
+        if (customer == null)
+            throw new KeyNotFoundException($"Customer with ID {id} not found.");
+        
+        _mapper.Map(customerDto, customer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteCustomerAsync(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        
+        if (customer == null)
+            throw new KeyNotFoundException($"Customer with ID {id} not found.");
+        
+        _context.Customers.Remove(customer);
+        await _context.SaveChangesAsync();
+    }
+}
