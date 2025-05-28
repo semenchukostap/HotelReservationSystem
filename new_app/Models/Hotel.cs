bc50a7f3c881144a04e37acda9325868c0883401
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 09:41:13 -0700

    Update Hotel model with .NET 8 conventions

--- new_app/Models/Hotel.cs
+++ new_app/Models/Hotel.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace HotelReservationSystem.Models;
 
@@ -8,8 +10,10 @@ public class Hotel
 
     [Required]
     [MaxLength(255)]
+    [Display(Name = "Hotel Name")]
     public string Name { get; set; } = string.Empty;
 
+    [ForeignKey("CountryId")]
     public Country? Country { get; set; }
 
     [Required]
@@ -18,16 +22,36 @@ public class Hotel
 
     [Required]
     [MaxLength(50)]
+    [Display(Name = "City")]
     public string City { get; set; } = string.Empty;
 
     [Required]
     [Range(1, 5)]
+    [Display(Name = "Star Rating")]
     public int Stars { get; set; }
 
     [Required]
     [Range(1, 1000)]
-    public double PricePerNight { get; set; }
+    [Display(Name = "Price Per Night")]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal PricePerNight { get; set; }
 
     [Required]
+    [Display(Name = "All Inclusive")]
     public bool IsAllInclusive { get; set; }
+
+    [MaxLength(500)]
+    [Display(Name = "Description")]
+    public string? Description { get; set; }
+
+    [Display(Name = "Available Rooms")]
+    [Range(0, 1000)]
+    public int AvailableRooms { get; set; }
+
+    // Navigation properties
+    public virtual ICollection<Reservation>? Reservations { get; set; }
+    
+    [NotMapped]
+    public bool IsAvailable => AvailableRooms > 0;
 }
