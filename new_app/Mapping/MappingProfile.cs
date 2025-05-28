2132849e4bfa208ab4dae7966429bc0474d6b848
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:14:16 -0700

    Create MappingProfile for AutoMapper configuration

---
+++ new_app/Mapping/MappingProfile.cs
@@ -0,0 +1,23 @@
+using AutoMapper;
+using HotelReservationSystem.DTOs;
+using HotelReservationSystem.Models;
+
+namespace HotelReservationSystem.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to DTO
+            CreateMap<Hotel, HotelDto>()
+                .ForMember(dto => dto.CountryName, opt => opt.MapFrom(h => h.Country != null ? h.Country.Name : string.Empty));
+                
+            CreateMap<Country, CountryDto>();
+            
+            // DTO to Domain
+            CreateMap<HotelDto, Hotel>();
+            CreateMap<CountryDto, Country>();
+            CreateMap<NewOrderDto, Order>();
+        }
+    }
+}
