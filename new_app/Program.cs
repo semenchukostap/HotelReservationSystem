0543de30f71c02e699f9ac8454e58e347a31fd6f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:06:02 -0700

    QA: Update Program.cs for .NET 8 application

--- new_app/Program.cs
+++ new_app/Program.cs
@@ -70,25 +70,4 @@ app.MapControllerRoute(
 // Add WebAPI routes
 app.MapControllers();
 
-// Seed the database
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        // Apply migrations
-        var dbContext = services.GetRequiredService<ApplicationDbContext>();
-        await dbContext.Database.MigrateAsync();
-
-        // Seed roles and admin user
-        await DataSeeding.SeedRolesAndUsersAsync(app.Services);
-        await DataSeeding.SeedCountriesAsync(app.Services);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
-    }
-}
-
 app.Run();
