930dcd74ee4836d26626f556f3b88c756aaca13f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 03:10:26 -0700

    Add ApplicationDbContext for .NET 8 migration

---
+++ new_app/HotelReservationSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+using HotelReservationSystem.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservationSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configure entity relationships and constraints here
+            builder.Entity<Hotel>()
+                .HasOne(h => h.Country)
+                .WithMany()
+                .HasForeignKey(h => h.CountryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Order>()
+                .HasOne(o => o.Customer)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Order>()
+                .HasOne(o => o.Hotel)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
