88730da5f515f1bfbfd0c13a9e38c1ec5d47a647
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 13:19:20 -0700

    QA: Add missing using statement for IConfiguration

--- new_app/Controllers/API/NewOrdersController.cs
+++ new_app/Controllers/API/NewOrdersController.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace HotelReservationSystem.Controllers.API
 {
