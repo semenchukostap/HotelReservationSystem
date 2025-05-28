f1dba68605a36a574cd8c0ad3431edde39d576b7
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:45:05 -0700

    Migrate NewOrderDto class to the new_app project

---
+++ new_app/DTOs/NewOrderDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace new_app.DTOs
+{
+    public class NewOrderDto
+    {
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int HotelId { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public int NumberOfDays { get; set; }
+    }
+}
