b28a3f842c6712a709fd3da30d065b9273dedb7d
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 09:40:00 -0700

    Update MappingProfile.cs to use modern AutoMapper syntax for .NET 8

--- new_app/MappingProfile.cs
+++ new_app/MappingProfile.cs
@@ -9,19 +9,35 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         // Create maps for entities to DTOs and vice versa
-        CreateMap<Hotel, HotelDto>();
-        CreateMap<HotelDto, Hotel>();
+        CreateMap<Hotel, HotelDto>()
+            .ForMember(dest => dest.Country, opt => opt.ExplicitExpansion());
+        CreateMap<HotelDto, Hotel>()
+            .ForMember(dest => dest.Country, opt => opt.Ignore())
+            .ForMember(dest => dest.Orders, opt => opt.Ignore());
         
-        CreateMap<Country, CountryDto>();
-        CreateMap<CountryDto, Country>();
+        CreateMap<Country, CountryDto>()
+            .ForMember(dest => dest.Hotels, opt => opt.ExplicitExpansion());
+        CreateMap<CountryDto, Country>()
+            .ForMember(dest => dest.Hotels, opt => opt.Ignore());
         
         CreateMap<Order, NewOrderDto>();
         CreateMap<NewOrderDto, Order>()
-            .ForMember(dest => dest.DateOrdered, opt => opt.MapFrom(src => DateTime.Now))
-            .ForMember(dest => dest.FullPrice, opt => opt.Ignore());
+            .ForMember(dest => dest.DateOrdered, opt => opt.MapFrom(_ => DateTime.UtcNow))
+            .ForMember(dest => dest.FullPrice, opt => opt.Ignore())
+            .ForMember(dest => dest.Customer, opt => opt.Ignore())
+            .ForMember(dest => dest.Hotel, opt => opt.Ignore());
+
+        CreateMap<Order, OrderDto>()
+            .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Customer.FullName))
+            .ForMember(dest => dest.HotelName, opt => opt.MapFrom(src => src.Hotel.Name));
+        CreateMap<OrderDto, Order>()
+            .ForMember(dest => dest.Customer, opt => opt.Ignore())
+            .ForMember(dest => dest.Hotel, opt => opt.Ignore());
         
-        // Add more mappings as needed for Customer and other entities
-        CreateMap<Customer, CustomerDto>();
-        CreateMap<CustomerDto, Customer>();
+        // Mappings for Customer
+        CreateMap<Customer, CustomerDto>()
+            .ForMember(dest => dest.Orders, opt => opt.ExplicitExpansion());
+        CreateMap<CustomerDto, Customer>()
+            .ForMember(dest => dest.Orders, opt => opt.Ignore());
     }
 }
