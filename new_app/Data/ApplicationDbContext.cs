d6dedadaf9a6a461d9c443e96577ec31e9f26ce6
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:14:25 -0700

    QA: Update ApplicationDbContext to use Entity Framework Core with dependency injection

--- new_app/Data/ApplicationDbContext.cs
+++ new_app/Data/ApplicationDbContext.cs
@@ -10,6 +10,10 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Hotel> Hotels { get; set; } = null!;
     public DbSet<Country> Countries { get; set; } = null!;
     public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<Room> Rooms { get; set; } = null!;
+    public DbSet<RoomType> RoomTypes { get; set; } = null!;
+    public DbSet<Reservation> Reservations { get; set; } = null!;
+    public DbSet<Payment> Payments { get; set; } = null!;
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
@@ -20,6 +24,31 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         base.OnModelCreating(builder);
         
-        // Entity configurations can be added here
+        // Entity configurations
+        builder.Entity<Room>()
+            .HasOne(r => r.Hotel)
+            .WithMany(h => h.Rooms)
+            .HasForeignKey(r => r.HotelId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Room>()
+            .HasOne(r => r.RoomType)
+            .WithMany(rt => rt.Rooms)
+            .HasForeignKey(r => r.RoomTypeId);
+
+        builder.Entity<Reservation>()
+            .HasOne(r => r.Customer)
+            .WithMany(c => c.Reservations)
+            .HasForeignKey(r => r.CustomerId);
+
+        builder.Entity<Reservation>()
+            .HasOne(r => r.Room)
+            .WithMany(r => r.Reservations)
+            .HasForeignKey(r => r.RoomId);
+
+        builder.Entity<Payment>()
+            .HasOne(p => p.Reservation)
+            .WithOne(r => r.Payment)
+            .HasForeignKey<Payment>(p => p.ReservationId);
     }
 }
