095fc7864b2e4b3097b5e4ce7df7a7595bef4f6c
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 08:49:04 -0700

    Add generic Repository implementation

---
+++ new_app/HotelReservationSystem.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+using HotelReservationSystem.Core.Interfaces;
+using HotelReservationSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
