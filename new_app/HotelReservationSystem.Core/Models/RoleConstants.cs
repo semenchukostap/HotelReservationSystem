23d10fb50e9855f27b884be8526d22e14a7a48a9
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 03:09:18 -0700

    Add RoleConstants class for .NET 8 migration

---
+++ new_app/HotelReservationSystem.Core/Models/RoleConstants.cs
@@ -0,0 +1,8 @@
+namespace HotelReservationSystem.Core.Models
+{
+    public static class RoleConstants
+    {
+        public const string Admin = "Admin";
+        public const string CanManageHotels = "CanManageHotels";
+    }
+}
