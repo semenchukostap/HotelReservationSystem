7da674ae7777efa654d9bcbca89f9f4901100df9
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:43:01 -0700

    Migrate CountryDto class to new_app with .NET 8 conventions

---
+++ new_app/DTOs/CountryDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace new_app.DTOs
+{
+    public class CountryDto
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
