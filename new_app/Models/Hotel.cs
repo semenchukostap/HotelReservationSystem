419a0a093333dc4379c462378b3755af01790ee7
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 09:46:56 -0700

    Add Hotel model

---
+++ new_app/Models/Hotel.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace HotelReservationSystem.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public required string Name { get; set; }
+
+        [ValidateNever]
+        public Country? Country { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        public int CountryId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public required string City { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Stars { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        [Display(Name = "Price Per Night")]
+        public double PricePerNight { get; set; }
+
+        [Required]
+        [Display(Name = "All Inclusive")]
+        public bool IsAllInclusive { get; set; }
+    }
+}
