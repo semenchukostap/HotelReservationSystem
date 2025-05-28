69047a33df0021e9f0ca5e7673bf61ed20a52a9e
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 03:33:41 -0700

    QA: Update HomeController to follow ASP.NET Core conventions

--- new_app/HotelReservationSystem.Web/Controllers/HomeController.cs
+++ new_app/HotelReservationSystem.Web/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
+using System.Diagnostics;
 using HotelReservationSystem.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace HotelReservationSystem.Web.Controllers
 {
