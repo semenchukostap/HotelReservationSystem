db2bd6e512c0efdc5da0ec880396dee9b7864f26
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 03:15:21 -0700

    Add HotelsController for .NET 8 migration

---
+++ new_app/HotelReservationSystem.Web/Controllers/HotelsController.cs
@@ -0,0 +1,122 @@
+using HotelReservationSystem.Core.DTOs;
+using HotelReservationSystem.Core.Models;
+using HotelReservationSystem.Services;
+using HotelReservationSystem.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservationSystem.Web.Controllers
+{
+    public class HotelsController : Controller
+    {
+        private readonly IHotelService _hotelService;
+        private readonly ICountryService _countryService;
+
+        public HotelsController(IHotelService hotelService, ICountryService countryService)
+        {
+            _hotelService = hotelService;
+            _countryService = countryService;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            if (User.IsInRole(RoleConstants.Admin))
+                return View("List");
+
+            return View("ReadOnlyList");
+        }
+
+        [Authorize(Policy = "CanManageHotels")]
+        public async Task<IActionResult> New()
+        {
+            var countries = await _countryService.GetAllCountriesEntitiesAsync();
+
+            var viewModel = new HotelViewModel
+            {
+                Hotel = new Hotel(),
+                Countries = countries
+            };
+
+            return View("Form", viewModel);
+        }
+
+        [Authorize(Policy = "CanManageHotels")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var hotel = await _hotelService.GetHotelEntityByIdAsync(id);
+
+            if (hotel == null)
+                return NotFound();
+
+            var viewModel = new HotelViewModel
+            {
+                Hotel = hotel,
+                Countries = await _countryService.GetAllCountriesEntitiesAsync()
+            };
+
+            return View("Form", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "CanManageHotels")]
+        public async Task<IActionResult> Save(Hotel hotel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new HotelViewModel
+                {
+                    Hotel = hotel,
+                    Countries = await _countryService.GetAllCountriesEntitiesAsync()
+                };
+
+                return View("Form", viewModel);
+            }
+
+            var hotelDto = new HotelDto
+            {
+                Id = hotel.Id,
+                Name = hotel.Name,
+                City = hotel.City,
+                CountryId = hotel.CountryId,
+                IsAllInclusive = hotel.IsAllInclusive,
+                PricePerNight = hotel.PricePerNight,
+                Stars = hotel.Stars
+            };
+
+            if (hotel.Id == 0)
+                await _hotelService.CreateHotelAsync(hotelDto);
+            else
+                await _hotelService.UpdateHotelAsync(hotel.Id, hotelDto);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> NewCountry()
+        {
+            return View("NewCountryForm", new Country());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "CanManageHotels")]
+        public async Task<IActionResult> SaveCountry(Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("NewCountryForm", country);
+            }
+
+            var countryDto = new CountryDto
+            {
+                Id = country.Id,
+                Name = country.Name
+            };
+
+            await _countryService.CreateCountryAsync(countryDto);
+
+            return RedirectToAction("New");
+        }
+    }
+}
