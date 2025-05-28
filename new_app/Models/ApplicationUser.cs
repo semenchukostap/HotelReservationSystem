6d6763d659840f1bf8f09ba1abf14a6a1da4b965
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 09:47:30 -0700

    Add ApplicationUser model

---
+++ new_app/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelReservationSystem.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(20)]
+        public required string Phone { get; set; }
+    }
+}
