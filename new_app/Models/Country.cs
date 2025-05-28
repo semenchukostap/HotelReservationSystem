46f226b4a1edb8b673ea240674567c9a1679363c
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 10:19:17 -0700

    Update Country model for .NET 8 compatibility

--- new_app/Models/Country.cs
+++ new_app/Models/Country.cs
@@ -3,14 +3,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotelReservationSystem.Models;
 
+/// <summary>
+/// Represents a country in the system
+/// </summary>
 [Table("Countries")]
 public class Country
 {
+    /// <summary>
+    /// The unique identifier for the country
+    /// </summary>
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     
-    [Required]
-    [StringLength(100)]
+    /// <summary>
+    /// The name of the country
+    /// </summary>
+    [Required(ErrorMessage = "Country name is required")]
+    [StringLength(60, ErrorMessage = "Country name cannot exceed 60 characters")]
+    [Display(Name = "Country Name")]
     public required string Name { get; set; } = string.Empty;
 }
