e9edae11faca33266dd0a739387a188d95c407aa
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-28 08:44:34 -0700

    Create CountryDto class

---
+++ new_app/src/HotelReservationSystem.Core/DTOs/CountryDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Core.DTOs;
+
+public class CountryDto
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(255)]
+    public string Name { get; set; } = string.Empty;
+}
