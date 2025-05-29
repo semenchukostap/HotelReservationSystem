2736baade33c9af2b5699ebe2e5941659848422f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 08:24:53 -0700

    Create Country model with .NET 8 features

---
+++ new_app/Models/Country.cs
@@ -0,0 +1,9 @@
+namespace HotelReservationSystem.Models;
+
+public class Country
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    
+    public ICollection<Hotel>? Hotels { get; set; }
+}
