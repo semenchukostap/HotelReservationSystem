099127ec3e2c87efacd9e41a98454f9ac3031c2b
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 04:19:11 -0700

    QA: Update MappingProfiles class to .NET 8 standards

--- new_app/MappingProfiles.cs
+++ new_app/MappingProfiles.cs
@@ -4,12 +4,20 @@ using HotelReservationSystem.Models;
 
 namespace HotelReservationSystem
 {
+    /// <summary>
+    /// Defines AutoMapper mappings for the hotel reservation system
+    /// </summary>
     public class MappingProfiles : Profile
     {
         public MappingProfiles()
         {
+            // Map domain entities to DTOs
             CreateMap<Hotel, HotelDto>();
             CreateMap<Country, CountryDto>();
+            
+            // Map DTOs to domain entities (bidirectional mapping)
+            CreateMap<HotelDto, Hotel>();
+            CreateMap<CountryDto, Country>();
         }
     }
 }
