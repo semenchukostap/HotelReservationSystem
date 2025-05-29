f40edaabbeb2f8ac490f9adba7ad9253d5d454a0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 03:09:21 -0700

    Migrate FacebookData from legacy app

---
+++ new_app/Hidden/FacebookData.cs
@@ -0,0 +1,8 @@
+namespace new_app.Hidden
+{
+    public static class FacebookData
+    {
+        public const string AppId = "your-facebook-app-id";
+        public const string AppSecret = "your-facebook-app-secret";
+    }
+}
