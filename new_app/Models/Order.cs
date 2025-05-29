12f797a1656fe67493c06352d33de072a3f27764
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 05:28:01 -0700

    Migrate Order model to .NET 8 standards
    - Replace direct entity references with foreign keys (CustomerId, HotelId)
    - Update type precision from double to decimal for FullPrice
    - Add data annotations for required fields and validation
    - Update navigation properties to be nullable (?)
    - Rename date fields to be more descriptive (CheckInDate, CheckOutDate)
    - Remove redundant NumberOfDays (can be calculated)

--- new_app/Models/Order.cs
+++ new_app/Models/Order.cs
@@ -1,15 +1,38 @@
 namespace HotelReservationSystem.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Order
 {
+    [Key]
     public int Id { get; set; }
-    public required string UserId { get; set; }
+
+    [Required]
+    public required string CustomerId { get; set; }
+
+    [Required]
     public required int HotelId { get; set; }
-    public required DateTime CheckIn { get; set; }
-    public required DateTime CheckOut { get; set; }
-    public decimal TotalPrice { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    [Display(Name = "Check-in Date")]
+    public required DateTime CheckInDate { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    [Display(Name = "Check-out Date")]
+    public required DateTime CheckOutDate { get; set; }
+
+    [Required]
+    [Range(0, 999999.99)]
+    [DataType(DataType.Currency)]
+    [Display(Name = "Full Price")]
+    public decimal FullPrice { get; set; }
+
+    [Required]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    
-    public ApplicationUser User { get; set; } = null!;
-    public Hotel Hotel { get; set; } = null!;
+
+    // Navigation properties
+    public Customer? Customer { get; set; }
+    public Hotel? Hotel { get; set; }
 }
