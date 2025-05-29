ea4c46781bce3e86c40fb7534ec303283cb4cc49
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 05:27:04 -0700

    QA: Add LoginViewModel with modern validation attributes

---
+++ new_app/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationSystem.Web.ViewModels;
+
+public class LoginViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "Remember me?")]
+    public bool RememberMe { get; set; }
+}
