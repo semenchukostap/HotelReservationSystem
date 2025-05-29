dfff42cf84a249baedfad1794ee3a786c9eb089e
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 05:29:08 -0700

    refactor(data): Update ApplicationDbContext with modern .NET 8 features
    - Added null reference annotations
    - Configured relationships with DeleteBehavior.Restrict
    - Added property configurations with max lengths
    - Set decimal precision for price fields
    - Improved type safety and data integrity

--- new_app/Data/ApplicationDbContext.cs
+++ new_app/Data/ApplicationDbContext.cs
@@ -11,27 +11,83 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
     }
 
-    public DbSet<Hotel> Hotels => Set<Hotel>();
-    public DbSet<Country> Countries => Set<Country>();
-    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<Hotel> Hotels { get; set; } = null!;
+    public DbSet<Country> Countries { get; set; } = null!;
+    public DbSet<Order> Orders { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<Hotel>()
-            .HasOne(h => h.Country)
-            .WithMany(c => c.Hotels)
-            .HasForeignKey(h => h.CountryId);
+        builder.Entity<Hotel>(entity =>
+        {
+            entity.HasOne(h => h.Country)
+                .WithMany(c => c.Hotels)
+                .HasForeignKey(h => h.CountryId)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        builder.Entity<Order>()
-            .HasOne(o => o.User)
-            .WithMany(u => u.Orders)
-            .HasForeignKey(o => o.UserId);
+            entity.Property(h => h.Name)
+                .IsRequired()
+                .HasMaxLength(200);
 
-        builder.Entity<Order>()
-            .HasOne(o => o.Hotel)
-            .WithMany(h => h.Hotels)
-            .HasForeignKey(o => o.HotelId);
+            entity.Property(h => h.Description)
+                .HasMaxLength(2000);
+
+            entity.Property(h => h.PricePerNight)
+                .HasPrecision(18, 2)
+                .IsRequired();
+
+            entity.Property(h => h.Rating)
+                .HasPrecision(3, 1);
+        });
+
+        builder.Entity<Country>(entity =>
+        {
+            entity.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(c => c.Code)
+                .IsRequired()
+                .HasMaxLength(2);
+        });
+
+        builder.Entity<Order>(entity =>
+        {
+            entity.HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasOne(o => o.Hotel)
+                .WithMany(h => h.Hotels)
+                .HasForeignKey(o => o.HotelId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.Property(o => o.CheckInDate)
+                .IsRequired();
+
+            entity.Property(o => o.CheckOutDate)
+                .IsRequired();
+
+            entity.Property(o => o.TotalPrice)
+                .HasPrecision(18, 2)
+                .IsRequired();
+
+            entity.Property(o => o.Status)
+                .IsRequired()
+                .HasMaxLength(50);
+        });
+
+        builder.Entity<ApplicationUser>(entity =>
+        {
+            entity.Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+        });
     }
 }
