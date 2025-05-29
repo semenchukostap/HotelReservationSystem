3246d96db9783e19147f45bb209a3cf78daf7252
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 03:09:47 -0700

    Migrate AdminCredentials from legacy app

---
+++ new_app/Hidden/AdminCredentials.cs
@@ -0,0 +1,8 @@
+namespace new_app.Hidden
+{
+    public static class AdminCredentials
+    {
+        public const string AdminEmail = "admin@example.com";
+        public const string AdminPassword = "MyStrongPassword123!";
+    }
+}
