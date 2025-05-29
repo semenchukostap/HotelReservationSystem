e9b9df5b43a9c49a29d36caa51dcbb281928fa90
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 07:57:11 -0700

    update_program_cs_with_enhanced_static_files_configuration

--- new_app/HotelReservationSystem/Program.cs
+++ new_app/HotelReservationSystem/Program.cs
@@ -4,6 +4,8 @@ using HotelReservationSystem.Data;
 using HotelReservationSystem.Models;
 using HotelReservationSystem.Services;
 using HotelReservationSystem.Helpers;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace HotelReservationSystem;
 
@@ -90,7 +92,31 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-        app.UseStaticFiles();
+
+        // Configure Static Files Middleware
+        // This middleware enables serving static files like CSS, JavaScript, images, etc.
+        // It must be called before other middleware that might use static files (like UseRouting)
+        // The default directory is 'wwwroot', but additional directories can be specified
+        app.UseStaticFiles(); 
+        
+        // Optional: Configure additional static file directories if needed
+        // This allows serving Bootstrap CSS and other library files from external paths
+        /* 
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(
+                Path.Combine(builder.Environment.ContentRootPath, "node_modules")),
+            RequestPath = "/lib"
+        });
+        */
+
+        // Optional: Configure default document serving behavior
+        /* 
+        app.UseDefaultFiles(new DefaultFilesOptions
+        {
+            DefaultFileNames = new List<string> { "index.html", "default.html" }
+        });
+        */
 
         app.UseRouting();
 
