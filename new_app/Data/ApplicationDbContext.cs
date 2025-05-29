b5e7926479c59e1e42edc0b8e9cb241a1862698f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 04:24:48 -0700

    QA: Update ApplicationDbContext to replace IdentityModels.cs

--- new_app/Data/ApplicationDbContext.cs
+++ new_app/Data/ApplicationDbContext.cs
@@ -6,10 +6,10 @@ namespace HotelReservationSystem.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Hotel> Hotels { get; set; }
-        public DbSet<Country> Countries { get; set; }
-        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Hotel> Hotels { get; set; } = null!;
+        public DbSet<Country> Countries { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
         
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
