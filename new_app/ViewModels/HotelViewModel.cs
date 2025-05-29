803e4709b08f624912e43da9fd4d239df229e2bb
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 08:27:22 -0700

    Add HotelViewModel for .NET 8 migration
    This commit adds the HotelViewModel class to the new .NET 8 application.
    The ViewModel is updated to use modern C# features including:
    - Nullable reference types (Hotel? and IEnumerable<Country>?)
    - Clean namespace declarations (no curly braces)
    - Removal of unnecessary usings
    
    This ViewModel is used in forms where users need to select a country for a hotel.

---
+++ new_app/ViewModels/HotelViewModel.cs
@@ -0,0 +1,9 @@
+using HotelReservationSystem.Models;
+
+namespace HotelReservationSystem.ViewModels;
+
+public class HotelViewModel
+{
+    public Hotel? Hotel { get; set; }
+    public IEnumerable<Country>? Countries { get; set; }
+}
