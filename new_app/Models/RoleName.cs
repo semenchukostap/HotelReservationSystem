da5a1578dd86ebf5a9193e600c294a3a20a0724f
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 08:24:44 -0700

    Create RoleName.cs with role constants in .NET 8 format

---
+++ new_app/Models/RoleName.cs
@@ -0,0 +1,7 @@
+namespace HotelReservationSystem.Models;
+
+public static class RoleName
+{
+    public const string CanManageHotels = "CanManageHotels";
+    public const string Admin = "Admin";
+}
