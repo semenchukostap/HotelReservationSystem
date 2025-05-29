cc23ea9ba22cd168b3743baac53226955c6f9262
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 05:21:46 -0700

    Add ApplicationUser model

---
+++ new_app/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelReservationSystem.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+}
