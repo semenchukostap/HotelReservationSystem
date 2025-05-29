2fea94ae7672c92178f84d2508e1da75c1598568
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 05:26:37 -0700

    feat(Models): Migrate Hotel model to .NET 8
    - Added required keyword for non-nullable string properties
    - Added data annotations for validation
    - Added navigation property for Orders collection
    - Updated property types to match .NET 8 conventions
    - Maintained existing validation rules
    - Added XML documentation

--- new_app/Models/Hotel.cs
+++ new_app/Models/Hotel.cs
@@ -1,12 +1,34 @@
 namespace HotelReservationSystem.Models;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Hotel
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Display(Name = "Hotel Name")]
     public required string Name { get; set; }
+
+    [Required]
+    [StringLength(200)]
     public required string Address { get; set; }
+
+    [Required]
+    [ForeignKey("Country")]
+    [Display(Name = "Country")]
     public required int CountryId { get; set; }
-    public Country Country { get; set; } = null!;
+
+    public virtual Country Country { get; set; } = null!;
+
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    [Display(Name = "Price Per Night")]
+    [Range(0, 999999.99)]
     public decimal PricePerNight { get; set; }
-    public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
