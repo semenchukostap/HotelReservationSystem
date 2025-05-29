c938e690b39e6a78f2e00727544112049b955948
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 03:33:17 -0700

    Update RoleName class with simplified role definition

--- new_app/Models/RoleName.cs
+++ new_app/Models/RoleName.cs
@@ -1,7 +1,7 @@
-namespace new_app.Models
+namespace HotelReservationSystem.Models
 {
     public static class RoleName
     {
-        public const string CanManageHotels = "CanManageHotels";
+        public const string Admin = "Admin";
     }
 }
