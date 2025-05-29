f20139d3edc497afcaafb345256b9b135b8859f1
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-29 08:27:16 -0700

    Add NewOrderDto class for order creation in .NET 8 migration

---
+++ new_app/DTOs/NewOrderDto.cs
@@ -0,0 +1,9 @@
+namespace HotelReservationSystem.DTOs;
+
+public class NewOrderDto
+{
+    public int CustomerId { get; set; }
+    public int HotelId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
