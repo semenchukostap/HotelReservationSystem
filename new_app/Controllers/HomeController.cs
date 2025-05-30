9922b950b2fe3e9b7526b75b1dea533bf785ce84
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 00:27:17 -0700

    Add HomeController for ASP.NET Core MVC

---
+++ new_app/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using HotelReservationSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HotelReservationSystem.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
