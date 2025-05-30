033b3214060eb3808a2e7891543fd820cfd74594
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 10:25:00 -0700

    Update Order model with modern C# features, nullable reference types, and renamed properties to match .NET 8 conventions

---
+++ new_app/Models/Order.cs
@@ -0,0 +1,28 @@
+namespace HotelReservationSystem.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public Customer? Customer { get; set; }
+        
+        [Required]
+        public int CustomerId { get; set; }
+        
+        public Hotel? Hotel { get; set; }
+        
+        [Required]
+        public int HotelId { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public DateTime CheckIn { get; set; }
+
+        [Required]
+        public DateTime CheckOut { get; set; }
+
+        public double FullPrice { get; set; }
+    }
+}
