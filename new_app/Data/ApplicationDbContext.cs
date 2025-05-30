f1e7beb01d4b1e91a4c5c334761014549a9f8038
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 00:44:23 -0700

    QA: Update ApplicationDbContext for .NET 8 with EF Core

--- new_app/Data/ApplicationDbContext.cs
+++ new_app/Data/ApplicationDbContext.cs
@@ -19,6 +19,35 @@ namespace HotelReservationSystem.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            // Configure entity relationships
+            builder.Entity<Hotel>()
+                .HasOne(h => h.Country)
+                .WithMany(c => c.Hotels)
+                .HasForeignKey(h => h.CountryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Order>()
+                .HasOne(o => o.Customer)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.CustomerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Order>()
+                .HasOne(o => o.Hotel)
+                .WithMany(h => h.Orders)
+                .HasForeignKey(o => o.HotelId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Configure decimal precision for price fields
+            builder.Entity<Hotel>()
+                .Property(h => h.PricePerNight)
+                .HasColumnType("decimal(18,2)");
+
+            builder.Entity<Order>()
+                .Property(o => o.TotalPrice)
+                .HasColumnType("decimal(18,2)");
+
             // Additional model configurations can be added here
         }
     }
