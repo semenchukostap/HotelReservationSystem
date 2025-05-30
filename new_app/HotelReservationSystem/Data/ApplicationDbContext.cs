0789cdfee51a3dd7ee069bd8a83def10b2ca60d2
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 14:30:59 -0700

    QA: Update ApplicationDbContext for EF Core 8

--- new_app/HotelReservationSystem/Data/ApplicationDbContext.cs
+++ new_app/HotelReservationSystem/Data/ApplicationDbContext.cs
@@ -6,10 +6,10 @@ namespace HotelReservationSystem.Data;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
-    public DbSet<Customer> Customers { get; set; } = null!;
-    public DbSet<Hotel> Hotels { get; set; } = null!;
-    public DbSet<Country> Countries { get; set; } = null!;
-    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+    public DbSet<Country> Countries => Set<Country>();
+    public DbSet<Order> Orders => Set<Order>();
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
@@ -21,5 +21,35 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         base.OnModelCreating(builder);
         
         // Configure entity relationships and constraints here
+        
+        // Configure cascade delete behavior
+        builder.Entity<Customer>()
+            .HasMany(c => c.Orders)
+            .WithOne(o => o.Customer)
+            .OnDelete(DeleteBehavior.Restrict);
+            
+        builder.Entity<Hotel>()
+            .HasMany(h => h.Orders)
+            .WithOne(o => o.Hotel)
+            .OnDelete(DeleteBehavior.Restrict);
+            
+        builder.Entity<Country>()
+            .HasMany(c => c.Hotels)
+            .WithOne(h => h.Country)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+    
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+        
+        // Configure string properties to have a default max length
+        configurationBuilder.Properties<string>()
+            .AreUnicode()
+            .HaveMaxLength(256);
+            
+        // Configure DateTime properties to use the date-time mapping
+        configurationBuilder.Properties<DateTime>()
+            .HaveColumnType("datetime2");
     }
 }
