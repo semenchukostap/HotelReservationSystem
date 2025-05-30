0a17d661f43c97d5119f21c8db58bcd9cead2c58
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 14:42:15 -0700

    QA: Update CountryDto

--- new_app/HotelReservationSystem/DTOs/CountryDto.cs
+++ new_app/HotelReservationSystem/DTOs/CountryDto.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace HotelReservationSystem.DTOs;
 
 public class CountryDto
@@ -7,5 +5,5 @@ public class CountryDto
     public int Id { get; set; }
     
     [Required]
-    public string Name { get; set; } = string.Empty;
+    public required string Name { get; set; } = string.Empty;
 }
