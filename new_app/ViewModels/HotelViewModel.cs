4399a74b833ccdff119e8eb9768ec657f0efc71d
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 10:26:06 -0700

    Add HotelViewModel with modern C# features and nullable reference types

---
+++ new_app/ViewModels/HotelViewModel.cs
@@ -0,0 +1,8 @@
+namespace HotelReservationSystem.ViewModels
+{
+    public class HotelViewModel
+    {
+        public Hotel Hotel { get; set; } = new Hotel();
+        public IEnumerable<Country> Countries { get; set; } = new List<Country>();
+    }
+}
