5e85190622ebc8da518f249513c2795088ed71e1
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 13:56:39 -0700

    Create ApplicationUser class for .NET 8 with modern syntax and nullability support

---
+++ new_app/HotelReservationSystem/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelReservationSystem.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    [Required]
+    [MaxLength(20)]
+    public string Phone { get; set; } = string.Empty;
+}
