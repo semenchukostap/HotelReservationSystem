27e43f35a508ba3b45249270c9ad9867bf284ef7
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 10:25:04 -0700

    Migrate CountryDto.cs to .NET 8 with modern C# features and nullable reference types

---
+++ new_app/DTOs/CountryDto.cs
@@ -0,0 +1,9 @@
+namespace HotelReservationSystem.DTOs
+{
+    public class CountryDto
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; } = string.Empty;
+    }
+}
