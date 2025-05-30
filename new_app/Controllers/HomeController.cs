b40541c5a41a80a46afd168df114622f8ffa45a0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 10:28:11 -0700

    Migrate HomeController to ASP.NET Core MVC with modern conventions

---
+++ new_app/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HotelReservationSystem.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
