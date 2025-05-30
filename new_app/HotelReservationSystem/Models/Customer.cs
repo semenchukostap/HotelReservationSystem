6ff836977c028b1ce00967bfb110a596040af584
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 14:32:42 -0700

    QA: Update Customer model

--- new_app/HotelReservationSystem/Models/Customer.cs
+++ new_app/HotelReservationSystem/Models/Customer.cs
@@ -1,14 +1,14 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace HotelReservationSystem.Models;
 
 public class Customer
 {
     public int Id { get; set; }
 
-    [Required]
-    [MaxLength(255)]
-    public string Name { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Name is required")]
+    [MaxLength(255, ErrorMessage = "Name cannot exceed 255 characters")]
+    public required string Name { get; set; }
 
+    [DataType(DataType.Date)]
+    [Display(Name = "Date of Birth")]
     public DateTime? Birthdate { get; set; }
 }
