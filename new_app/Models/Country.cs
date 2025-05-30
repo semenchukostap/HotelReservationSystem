46a53529f1376634136e5d6d2619dcfc09955722
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 00:54:20 -0700

    QA: Finalize Country model migration with documentation

--- new_app/Models/Country.cs
+++ new_app/Models/Country.cs
@@ -1,7 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotelReservationSystem.Models
 {
+    /// <summary>
+    /// Represents a country entity in the hotel reservation system.
+    /// </summary>
     public class Country
     {
         public int Id { get; set; }
