f2b3c440bb46eb889a134a83cfd31aaf8430fc10
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 00:28:00 -0700

    Add API CustomersController for ASP.NET Core Web API

---
+++ new_app/Controllers/API/CustomersController.cs
@@ -0,0 +1,106 @@
+using HotelReservationSystem.Data;
+using HotelReservationSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservationSystem.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        [Authorize(Roles = RoleName.CanManageHotels)]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        [Authorize(Roles = RoleName.CanManageHotels)]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        [Authorize(Roles = RoleName.CanManageHotels)]
+        public async Task<ActionResult<Customer>> CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        [Authorize(Roles = RoleName.CanManageHotels)]
+        public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var customerInDb = await _context.Customers.FindAsync(id);
+
+            if (customerInDb == null)
+            {
+                return NotFound();
+            }
+
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthdate = customer.Birthdate;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        [Authorize(Roles = RoleName.CanManageHotels)]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
