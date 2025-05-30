1a2e2c3832ca36dee5066eb06e168962d1ae0825
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-05-30 14:08:37 -0700

    Add site.js file for client-side functionality

---
+++ new_app/HotelReservationSystem/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+// Common JavaScript functions
+$(document).ready(function () {
+    // Initialize DataTables
+    $('.table').DataTable();
+    
+    // Initialize Toastr notifications
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+});
