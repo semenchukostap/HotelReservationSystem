2c12a6f6576dff4c09983c0e35d9ac4058a68899
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-06-02 17:44:31 +0300

    Add ApplicationUser model for .NET 8 project

---
+++ new_app/HotelReservationSystem/Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelReservationSystem.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Phone { get; set; } = string.Empty;
+    }
+}
