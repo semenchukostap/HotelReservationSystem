5f2f07908c110e8651004cc7df15761714bcfc6a
AlexonEpamGitHub <135996907+AlexonEpamGitHub@users.noreply.github.com>
2025-06-02 17:44:21 +0300

    Add ApplicationDbContext for .NET 8 project

---
+++ new_app/HotelReservationSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using HotelReservationSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservationSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Hotel> Hotels { get; set; } = null!;
+        public DbSet<Country> Countries { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            // Add custom model configurations here
+        }
+    }
+}
