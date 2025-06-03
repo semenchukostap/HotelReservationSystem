53bd607eb5b5c2835b1d8c0f68caa7f4497df4ab
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-03 05:40:36 -0700

    QA: Update NewOrderDto for .NET 8

--- new_app/DTOs/NewOrderDto.cs
+++ new_app/DTOs/NewOrderDto.cs
@@ -1,10 +1,14 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace HotelReservationSystem.DTOs
 {
     public class NewOrderDto
     {
+        [Required]
         public int CustomerId { get; set; }
+        
+        [Required]
         public int HotelId { get; set; }
         
         [Required]
