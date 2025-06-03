b9953e6cda657742d1d9098d7a394f40ac6f2834
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-03 05:37:14 -0700

    QA: Update RoleName class for .NET 8 migration

--- new_app/Models/RoleName.cs
+++ new_app/Models/RoleName.cs
@@ -1,8 +1,7 @@
-namespace HotelReservationSystem.Models
+namespace HotelReservationSystem.Models;
+
+public static class RoleName
 {
-    public static class RoleName
-    {
-        public const string CanManageHotels = "CanManageHotels";
-        // Add other roles as needed
-    }
+    public const string CanManageHotels = "CanManageHotels";
+    // Add other roles as needed
 }
