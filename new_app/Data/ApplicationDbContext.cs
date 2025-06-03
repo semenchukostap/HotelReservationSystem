3c0e5dd00dc53b7977d0490419fcef4490230112
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-03 05:21:31 -0700

    Add ApplicationDbContext for Entity Framework Core

---
+++ new_app/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HotelReservationSystem.Models;
+
+namespace HotelReservationSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Hotel> Hotels { get; set; } = null!;
+        public DbSet<Country> Countries { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            // Configure your entity relationships here if needed
+        }
+    }
+}
