f499833a1a44596fbed85be56408be0cd65406ac
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-03 05:20:28 -0700

    Add RoleName class for role-based authorization

---
+++ new_app/Models/RoleName.cs
@@ -0,0 +1,8 @@
+namespace HotelReservationSystem.Models
+{
+    public static class RoleName
+    {
+        public const string CanManageHotels = "CanManageHotels";
+        // Add other roles as needed
+    }
+}
