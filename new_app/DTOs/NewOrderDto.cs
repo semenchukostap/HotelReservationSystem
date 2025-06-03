57f73bbfe44e042a6912851516360d5a97dc2ae7
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-03 04:28:43 -0700

    Update NewOrderDto.cs with proper System namespace and validation attributes

--- new_app/DTOs/NewOrderDto.cs
+++ new_app/DTOs/NewOrderDto.cs
@@ -1,20 +1,19 @@
-namespace new_app.DTOs
+namespace new_app.DTOs;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class NewOrderDto
 {
-    using System;
-    using System.ComponentModel.DataAnnotations;
+    [Required(ErrorMessage = "Customer ID is required")]
+    public required int CustomerId { get; set; }
 
-    public class NewOrderDto
-    {
-        [Required]
-        public int CustomerId { get; set; }
+    [Required(ErrorMessage = "Hotel ID is required")]
+    public required int HotelId { get; set; }
 
-        [Required]
-        public int HotelId { get; set; }
+    [Required(ErrorMessage = "Start date is required")]
+    public required DateOnly StartDate { get; set; }
 
-        [Required]
-        public DateTime StartDate { get; set; }
-
-        [Required]
-        public DateTime EndDate { get; set; }
-    }
+    [Required(ErrorMessage = "End date is required")]
+    public required DateOnly EndDate { get; set; }
 }
