8395dae30d957480ce60e45ac0ea8d7697d16ab0
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-04 03:06:14 -0700

    Add HotelViewModel for .NET 8 migration

---
+++ new_app/ViewModels/HotelViewModel.cs
@@ -0,0 +1,36 @@
+using new_app.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace new_app.ViewModels;
+
+public class HotelViewModel
+{
+    public int? Id { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [Display(Name = "Country")]
+    public int CountryId { get; set; }
+
+    public IEnumerable<Country>? Countries { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string City { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, 5)]
+    public int Stars { get; set; }
+
+    [Required]
+    [Range(1, 1000)]
+    public double PricePerNight { get; set; }
+
+    [Required]
+    public bool IsAllInclusive { get; set; }
+
+    public string Title => Id != 0 ? "Edit Hotel" : "New Hotel";
+}
