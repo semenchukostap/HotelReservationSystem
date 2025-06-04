5bf73683f2caf565857b8f78518aa5293d4fe08e
Ostap Semenchuk <72123138+semenchukostap@users.noreply.github.com>
2025-06-04 03:07:09 -0700

    Add HomeController for .NET 8 migration

---
+++ new_app/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using new_app.Models;
+using System.Diagnostics;
+
+namespace new_app.Controllers;
+
+[AllowAnonymous]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
